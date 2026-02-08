dcdbefe6e7e726607e6a4056da1030005f516a12
Drigger91 <82764734+Drigger91@users.noreply.github.com>
2026-02-08 22:29:44 +0530

    basic ttl changes

--- inMemoryDB/implementations/basic_key_value_store.go
+++ inMemoryDB/implementations/basic_key_value_store.go
@@ -3,37 +3,57 @@ package implementations
 import (
 	"reflect"
 	"sync"
+	"time"
 )
 
+type BasicKeyValueEntry struct {
+	value any
+	ttl int64
+}
 type BasicKeyValueStore[K comparable] struct {
-	store map[K]any // this will contain key - entry mappings
+	store map[K]BasicKeyValueEntry // this will contain key - entry mappings
 	rw sync.RWMutex
 }
-func NewBasicKeyValueStore[K comparable]() BasicKeyValueStore[K] {
-	return BasicKeyValueStore[K]{
-		store: make(map[K]any),
+func NewBasicKeyValueStore[K comparable]() *BasicKeyValueStore[K] {
+	return &BasicKeyValueStore[K]{
+		store: make(map[K]BasicKeyValueEntry),
 	}
 }
-func (kv *BasicKeyValueStore[K]) Set(key K, newValue any) (bool, error) {
+
+func (kv *BasicKeyValueStore[K]) SetEx(key K, newValue any, ttl time.Duration) (bool, error) {
 	kv.rw.Lock()
 	defer kv.rw.Unlock()
+	entryVal := BasicKeyValueEntry{
+		value: newValue,
+		ttl: time.Now().UnixNano() + ttl.Nanoseconds(),
+	}
 	if _, exists := kv.store[key]; !exists {
-		kv.store[key] = newValue
+		kv.store[key] = entryVal
 		return true, nil	
 	}
 	// already exist
-	val := kv.store[key]
+	val := kv.store[key].value
 	if reflect.TypeOf(val) != reflect.TypeOf(newValue) {
 		panic("Types are not matching for existing key")
 	}
-	kv.store[key] = newValue
+
+	kv.store[key] = entryVal
 	return true, nil
+
+}
+func (kv *BasicKeyValueStore[K]) Set(key K, newValue any) (bool, error) {
+	// setting 5 years as infinity for now
+	return kv.SetEx(key, newValue, 5 * time.Duration(time.Now().Year()))
 }
 
 func (kv *BasicKeyValueStore[K]) Get(key K) (any, bool) {
 	kv.rw.RLock()
 	defer kv.rw.RUnlock()
 	value, exists := kv.store[key]
+	// check if ttl is expired
+	if exists && time.Now().UnixNano() > value.ttl {
+		return BasicKeyValueEntry{}, false
+	}
 	return value, exists
 }
 func (db *BasicKeyValueStore[K]) Delete(key K) {
